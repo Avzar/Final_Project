efd98b262d0f230662a8bf0e133c3895b3aca5d2
Avzar <rassstinson@gmail.com>
2023-04-10 14:19:41 +0600

    Добавлено задание выполненное в c#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arr = { "apple", "banana", "cat", "dog", "egg", "fish", "goat" };
+
+        string[] newArr = new string[arr.Length];
+        int index = 0;
+        foreach (string s in arr)
+        {
+            if (s.Length <= 3)
+            {
+                newArr[index] = s;
+                index++;
+            }
+        }
+        Array.Resize(ref newArr, index);
+
+        Console.WriteLine(string.Join(", ", newArr));
+    }
+}
